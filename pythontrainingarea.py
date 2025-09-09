0bc7a3ccefeea6873adf6625863799b5abf79220
Muhammet Ali Erbas <iammuhametali@gmail.com>
2025-09-09 14:57:02 +0300

    tried cgpt

--- pythontrainingarea.py
+++ pythontrainingarea.py
@@ -1,1 +1,10 @@
 #allright, this is gonna be my python gym. 
+from openai import OpenAI
+client = OpenAI()
+
+response = client.responses.create(
+    model="gpt-5",
+    input="Write a short bedtime story about a unicorn."
+)
+
+print(response.output_text)
