f7abdb9f6e369828f8b0e2fe3465e540d2afa170
Muhammet Ali Erbas <iammuhametali@gmail.com>
2025-09-10 14:37:47 +0300

    ara commit

--- pythontrainingarea.py
+++ pythontrainingarea.py
@@ -1,4 +1,6 @@
 #allright, this is gonna be my python gym. 
+
+"""
 from openai import OpenAI
 client = OpenAI()
 
@@ -8,3 +10,30 @@ response = client.responses.create(
 )
 
 print(response.output_text)
+"""
+"""
+print(sorted(['abz','ds','zzb'], key=lambda x: x[-1]))
+"""
+
+"""
+print(list(filter(lambda x : x > 20 ,[10,15,20,25,30])))
+
+"""
+"""
+people = [('ali',25),('veli',20),('ayşe',22)]
+
+peoplesorted = sorted(people, key = lambda x : x[1])
+
+print(peoplesorted)
+
+print('        '.join(reversed('abc')))
+"""
+
+"""
+
+a = list("python")
+
+print("".join(reversed(a)))
+
+"""
+
